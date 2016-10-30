27ca92738c35ca2d97bb6193e7b248395997c522
AntonPon <ponomarchuk.anton@gmail.com>
2016-10-31 00:51:56 +0200

    add Project Service interface 2.1

--- src/main/java/nc/ukma/thor/spms/service/ProjectService.java
+++ src/main/java/nc/ukma/thor/spms/service/ProjectService.java
@@ -9,7 +9,7 @@ public interface ProjectService {
 /*
  ProjectService interface describes all functionality for projects
  */
-    boolean createProject(long projectId, String project);
+    boolean createProject(Project project);
     boolean deleteProject(long projectId);
     boolean changeProjectName(long projectId, String newProjectName);
 
@@ -31,30 +31,30 @@ public interface ProjectService {
     // team functionality
     boolean addTeam(long projectId, Team team);
     boolean addTeams(long projectId, List<Team> team);
-    boolean changeTeamName(long projectId, String team, String newName);
-    boolean deleteTeam(long projectId, String team);
+    boolean changeTeamName(long projectId, long teamId, String newName);
+    boolean deleteTeam(long projectId, long teamId);
 
-    Team getTeam(long projectId, String team);
+    Team getTeam(long projectId, long teamId);
     List<Team> getAllTeams(long projectId);
 
     // user
-    boolean addUser(long projectId, Team team, User user);
-    boolean addUsers(long projectId, List<User> team);
-    boolean deleteUser(long projectId, Team team, String user);
+    boolean addUser(long projectId, long teamId, User user);
+    boolean addUsers(long projectId, List<User> users);
+    boolean deleteUser(long projectId, long Id, String user);
 
     User getUser(long projectId, String user);
     List<User> getAllUsers(long projectId);
 
     // file functionality at the project
     boolean uploadFile(long projectId, File file);
-    boolean deleteFile(long projectId, String file);
-    File getFile(long projectId, String file);
+    boolean deleteFile(long projectId, long fileId);
+    File getFile(long projectId, long  fileId);
 
     // traits
     boolean setTraits(long projectId, List<Trait> traits);
     boolean setTrait(long projectId, Trait trait);
 
-    boolean deleteTrait(long projectId, Trait trait);
+    boolean deleteTrait(long projectId, long traitId);
     boolean deleteTraits(long projectId, List<Trait> traits);
     Trait getTrait(long projectId, long traitId);
     List<Trait> getTraits(long projectId);
