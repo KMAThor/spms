bdaaea7e35a9ad08424f8ae996d27f5de61038c0
Lida Pinchuk <lida.pinchuk.26@gmail.com>
2016-11-03 20:23:06 +0200

    Update TeamService interface with methods, add empty TeamServiceImpl

--- src/main/java/nc/ukma/thor/spms/service/TeamService.java
+++ src/main/java/nc/ukma/thor/spms/service/TeamService.java
@@ -1,5 +1,44 @@
 package nc.ukma.thor.spms.service;
 
+import nc.ukma.thor.spms.entity.*;
+import java.util.List;
+import java.util.Map;
+
+import javax.net.ssl.SSLEngineResult.Status;
+
+/*
+TeamService interface describes team functionality
+*/
 
 public interface TeamService {
+	
+    //team name
+    String getTeamName(long teamId);
+    
+    //team project
+    Project getProject(long teamId);
+    
+    //team members
+    boolean setMember(long teamId, long userId);
+    boolean setMembers(long teamId, Map<User, Status> members);
+    boolean addMember(long teamId, long userId);
+    boolean addMembers(long teamId, Map<User, Status> members);
+    boolean deleteMember(long teamId, long userId);
+    
+    User getMember(long teamId, long userId);
+    Map<User, Status> getMembers(long teamId);
+    
+    //team meetings
+    boolean setMeeting(long teamId, long meetingId);
+    boolean setMeetings(long teamId, List<Meeting> meetings);
+    boolean addMeeting(long teamId, long meetingId);
+    boolean addMeetings(long teamId, List<Meeting> meetings);
+    boolean deleteMeeting(long teamId, long meetingId);
+    
+    Meeting getMeeting(long teamId, long meetingId);
+    List<Meeting> getMeetings(long teamId);
+    
+    //team info
+    String getInfo(long teamId);
+	
 }
---
+++ src/main/java/nc/ukma/thor/spms/service/TeamServiceImpl.java
@@ -0,0 +1,10 @@
+package nc.ukma.thor.spms.service;
+
+import org.springframework.stereotype.Service;
+
+import nc.ukma.thor.spms.entity.Team;
+
+@Service
+public class TeamServiceImpl extends AbstractService<Team>{
+
+}
