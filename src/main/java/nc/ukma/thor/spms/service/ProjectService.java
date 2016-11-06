d30f7481d190e0c0b43f34e9ac7eb939d6744ba9
AntonPon <ponomarchuk.anton@gmail.com>
2016-11-06 17:30:04 +0200

    modified project service

--- src/main/java/nc/ukma/thor/spms/service/ProjectService.java
+++ src/main/java/nc/ukma/thor/spms/service/ProjectService.java
@@ -5,45 +5,29 @@ import nc.ukma.thor.spms.entity.*;
 import java.sql.Timestamp;
 import java.util.List;
 
-public interface ProjectService {
-/*
- ProjectService interface describes all functionality for projects
- */
-    boolean createProject(Project project);
-    boolean deleteProject(long projectId);
+public interface ProjectService{
+
     boolean changeProjectName(long projectId, String newProjectName);
 
     // timestamp
     boolean setTimeStamp(long projectId, boolean start, Timestamp date);
-    boolean changeTimeStamp(long projectId, boolean start, Timestamp date);
 
     //description of the project
     boolean setDescription(long projectId, String description);
-    boolean changeDescription(long projectId, String descritpion);
     boolean deleteDescription(long projectId);
 
     // chief mentor
     boolean setChiefUser(long projectId, User chief);
     boolean deleteChiefUser(long projectId);
-    boolean changeChiefUser(long projectId, User newChief);
 
 
     // team functionality
-    boolean addTeam(long projectId, Team team);
     boolean addTeams(long projectId, List<Team> team);
     boolean changeTeamName(long projectId, long teamId, String newName);
     boolean deleteTeam(long projectId, long teamId);
 
     Team getTeam(long projectId, long teamId);
     List<Team> getAllTeams(long projectId);
-
-    // user
-    boolean addUser(long projectId, long teamId, User user);
-    boolean addUsers(long projectId, List<User> users);
-    boolean deleteUser(long projectId, long Id, String user);
-
-    User getUser(long projectId, String user);
-    List<User> getAllUsers(long projectId);
 
     // file functionality at the project
     boolean uploadFile(long projectId, File file);
